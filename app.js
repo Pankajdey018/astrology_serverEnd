cf45f6c6dd42ce070bfe9ea39114fe186700ac32
Pankajdey018 <pankajdey.olduser@gmail.com>
2025-06-25 17:01:04 +0530

    added cors

--- app.js
+++ app.js
@@ -46,6 +46,9 @@
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+app.use(cors());
+
 // Import your router
 const astroRoutes = require("./Routes/astroRoutes");
 
